1a360ba8da10a7626fd5daf922d2e8af580b7870
gnanasindhu2007-debug <gnanasindhu2007@gmail.com>
2026-09-09 18:41:42 +0530

    Update model.py

--- ml-model/model.py
+++ ml-model/model.py
@@ -1,1 +1,15 @@
+import pickle
+import numpy as np
+from PIL import Image
 
+model = pickle.load(open("tomato_model.pkl", "rb"))
+
+def predict_disease(image_path):
+    img = Image.open(image_path)
+    img = img.resize((64, 64))
+
+    image = np.array(img).flatten().reshape(1, -1)
+
+    prediction = model.predict(image)
+
+    return prediction[0]
